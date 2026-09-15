c9acb8252cc3b49c83d9d799f807b9a1d055df80
chiaracariolato <chiaracariolato@gmail.com>
2026-09-15 21:24:16 +0200

    Fix: removed extra portal

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,36 +1,31 @@
 import { GlobalContext } from '../contexts/GlobalContext'
 import { useContext } from 'react'
-import { createPortal } from 'react-dom'
 
 export default function Sidebar({ isOpen, onClose }) {
   const { favourites = [] } = useContext(GlobalContext)
 
   return (
-    isOpen &&
-    createPortal(
-      <div className="offcanvas offcanvas-end show">
-        <div className="offcanvas-header">
-          <h5 className="offcanvas-title">
-            <i className="bi bi-heart-fill px-2" />
-            Favourites
-          </h5>
-          <button type="button" className="btn-close" onClick={onClose} aria-label="Close"></button>
-        </div>
-        <div className="offcanvas-body">
-          {favourites.length > 0 ? (
-            <ul className="list-group">
-              {favourites.map((product) => (
-                <li className="list-group-item" key={product.id}>
-                  {product.title}
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No favourites selected</p>
-          )}
-        </div>
-      </div>,
-      document.body,
-    )
+    <div className={`offcanvas offcanvas-end ${isOpen ? 'show' : ''}`}>
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title">
+          <i className="bi bi-heart-fill px-2" />
+          Favourites
+        </h5>
+        <button type="button" className="btn-close" onClick={onClose} aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body">
+        {favourites.length > 0 ? (
+          <ul className="list-group">
+            {favourites.map((product) => (
+              <li className="list-group-item" key={product.id}>
+                {product.title}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No favourites selected</p>
+        )}
+      </div>
+    </div>
   )
 }
